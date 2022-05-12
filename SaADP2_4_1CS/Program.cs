e548cda0b4611f7ac0104fb9e905e6a01e801baf
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-05-12 19:32:00 +0300

    Simple hash table.

--- SaADP2_4_1CS/Program.cs
+++ SaADP2_4_1CS/Program.cs
@@ -29,20 +29,115 @@ namespace SaADP2_4_1CS
                 Console.Write($"{i} - {hashTable[i]} , ");
             }
         }
-        public static bool KeyCheck(string key, string[] hashTable)
+        public static int KeySearch(string key, string[] hashTable)
         {
             if (hashTable[Hash(key)] == key)
             {
-                return true;
+                return Hash(key);
             }
-            return false;
+            return -1;
+        }
+
+        public static int Input()
+        {
+            string strInput; bool stop = false;
+            int number = -1;
+            while(!stop)
+            {
+                try
+                {
+                    strInput = Console.ReadLine();
+                    number = int.Parse(strInput); stop = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не верный ввод.");
+                    stop = false;
+                    throw;
+                }
+            }
+            return number;
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine(
+                "1. Заполнить хеш таблицу.\n"
+               +"2. Вывести хеш таблицу на экран.\n"
+               +"3. Найти ключ в хеш таблице.\n"
+               +"4. Завершить работу.");
+        }
+
+        public static void CasePrint(string[] hashTable)
+        {
+            if (!(hashTable[0] == null))
+            {
+                PrintHashTable(hashTable);
+            }
+            else
+                Console.WriteLine("Хеш таблица пуста.");
+        }
+
+        public static void CaseSearch(string[] hashTable)
+        {
+            if (!(hashTable[0] == null))
+            {
+                Console.Write("Введите ключ для поиска: ");
+                string key = Console.ReadLine();
+                int index = KeySearch(key, hashTable);
+                if (index == -1) { Console.WriteLine("Такого ключа нет."); }
+                else
+                    Console.WriteLine($"Ключ {key} в хеш таблице имеет место {index}.");
+            }
+            else
+                Console.WriteLine("Хеш таблица пуста.");
+        }
+
+        public static void ClearMemory(ref string[] hashTable, ref string[] keys)
+        {
+            for (int i = 0; i < arraySize; i++)
+            {
+                hashTable[i] = keys[i] = null;
+            }
+            hashTable = keys = null;
+        }
+
+        public static void Interface(ref string[] hashTable, ref string[] keys)
+        {
+            bool stop = false; PrintMenu();
+            while(!stop)
+            {
+                switch (Input())
+                {
+                    case 0:
+                        PrintMenu();
+                        break;
+                    case 1:
+                        CreateHashTable(ref hashTable, keys);
+                        break;
+                    case 2:
+                        CasePrint(hashTable);
+                        break;
+                    case 3:
+                        CaseSearch(hashTable);
+                        break;
+                    case 4:
+                        ClearMemory(ref hashTable, ref keys);
+                        stop = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Такого пункта меню нет.");
+                        break;
+                }
+                Console.WriteLine("0. Показать меню.");
+            }
         }
         static void Main(string[] args)
         {
             string[] keys = { "WHILE", "AND", "RETURN", "STRUct", "OUT", "Ref", "FOREACH", "PROGRAM", "THEN", "Class" };
             string[] hashTable = new string[arraySize];
-            CreateHashTable(ref hashTable, keys);
-            PrintHashTable(hashTable);
+            Interface(ref hashTable, ref keys);
         }
 
     }
